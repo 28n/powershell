d73927901409bb44ccb08e756b155944fae58eb9
28n <28nh04@gmail.com>
2022-12-21 21:09:25 +0100

    feat(git): modified git workflow

--- user_profile.ps1
+++ user_profile.ps1
@@ -1,6 +1,8 @@
 Import-Module posh-git
 
-function Get-ScriptDirectory { Split-Path $MyInvocation.ScriptName }
+function Get-ScriptDirectory
+{ Split-Path $MyInvocation.ScriptName 
+}
 $PROMPT_CONFIG = Join-Path (Get-ScriptDirectory) 'lambda.omp.json'
 # oh-my-posh init pwsh --config $PROMPT_CONFIG | Invoke-Expression
 
@@ -24,7 +26,7 @@ Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory
 $aliasdata = @(
   [pscustomobject]@{name = 'vim'; command = 'nvim'}
   [pscustomobject]@{name = 'vi'; command = 'nvim'}
-  [pscustomobject]@{name = 'g'; command = 'git'}
+  # [pscustomobject]@{name = 'g'; command = 'git'}
   [pscustomobject]@{name = 'll'; command = 'ls'}
   [pscustomobject]@{name = 'grep'; command = 'findstr'}
   [pscustomobject]@{name = 'tig'; command = 'C:\Program Files\Git\usr\bin\tig.exe'}
@@ -45,51 +47,127 @@ $aliasdata | ForEach-Object {
 # Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
 # Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
 
-function which ($command) {
-	Get-Command -Name $command -ErrorAction SilentlyContinue |
-	Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+function which ($command)
+{
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
 }
 
-function czinit () {
+function czinit ()
+{
   commitizen init cz-conventional-changelog --save-dev --save-exact
+}
+
+# Git Aliases
+
+function g ()
+{
+  param (
+    [string]$command,
+    [string]$arguments
+  )
+
+  $debug = $false
+
+  $command = switch ($command)
+  {
+    's'
+    { 
+      'status'
+    }
+    'a'
+    { 
+      'add'
+    }
+    default
+    { 
+      $command 
+    }
+  }
+  if ($command -ne '')
+  {
+
+
+    if ($arguments -ne '')
+    {
+      if ($debug -eq $true)
+      {
+        Write-Host 'Writing CMD with Args'
+      }
+
+      git $command $arguments
+    } else
+    {
+      if ($debug -eq $true)
+      {
+        Write-Host 'Writing CMD w/o Args'
+      }
+      git $command
+    }
+  } else
+  {
+    if ($debug -eq $true)
+    {
+      Write-Host 'Writing Git solo'
+    }
+
+    git
+  }
+  if ($debug -eq $true)
+  {
+    Write-Host 'Command:'
+    Write-Host $command
+    Write-Host 'Arguments:'
+    Write-Host $arguments
+  }
 }
 
 $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
 $runsAsAdmin = $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 
-function admin () {
-  if ($runsAsAdmin) {
+function admin ()
+{
+  if ($runsAsAdmin)
+  {
     # start powershell and echo hello world
     Write-Host "You are already running as Administrator"
     return
-  } else {
+  } else
+  {
     Start-Process wt.exe -Verb RunAs
   }
 }
 
-function isAdmin () {
-  if ($runsAsAdmin) {
+function isAdmin ()
+{
+  if ($runsAsAdmin)
+  {
     Write-Host "You are running as admin"
-  } else {
+  } else
+  {
     Write-Host "You are not running as admin"
   }
 }
 
-function author () {
-    Start-Process "https://www.samsierra.de"
+function author ()
+{
+  Start-Process "https://www.samsierra.de"
 }
 
-function tab () {
-    wt -w 0 nt
+function tab ()
+{
+  wt -w 0 nt
 }
 
-function cwdtab () {
-    wt -w 0 nt -d .
+function cwdtab ()
+{
+  wt -w 0 nt -d .
 }
 
 # CLS
 cls
 
-if ($starshipConfigPath -eq $false) {
+if ($starshipConfigPath -eq $false)
+{
   echo "The Starship config file does not exist. Please create one."
 }
