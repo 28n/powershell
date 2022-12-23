e8890a6c824a6f61a33625988433bcf9f6d73bf3
28n <28nh04@gmail.com>
2022-12-23 13:40:23 +0100

    feat(goto): init

--- user_profile.ps1
+++ user_profile.ps1
@@ -159,6 +159,43 @@ function g ()
   }
 }
 
+## Goto
+
+function gt
+{
+  param (
+    [string]$target
+  )
+
+  $target = switch ($target)
+  {
+    'h' # home
+    {
+      '~'
+    }
+    'nv' # nvim config
+    {
+      $ENV:LOCALAPPDATA + '\nvim\'
+    }
+    'nvd' # nvim-data
+    {
+      $ENV:LOCALAPPDATA + '\nvim-data\'
+    }
+    default
+    {
+      ''
+    }
+  }
+
+  if ($target -ne '')
+  {
+    Set-Location $target
+  } else
+  {
+    Write-Host 'No Target specified'
+  }
+}
+
 $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
 $runsAsAdmin = $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 
