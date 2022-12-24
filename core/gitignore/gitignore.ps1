a22a2090ebb591a581f98b20f0c8b2cb89ba8b3d
28n <28nh04@gmail.com>
2022-12-24 10:34:21 +0100

    feat(gitignore): init with 2 templates

--- core/gitignore/gitignore.ps1
+++ core/gitignore/gitignore.ps1
@@ -2,33 +2,74 @@ function gitignore
 {
   param (
     [switch]$help,
+    [switch]$show,
     [string]$type
   )
 
-  $possibletypes = @(
-    [pscustomobject]@{name = 'npm'; description = 'Minimal Gitignore that includes: package-locks and node_modules'}
+  $possibletypes = 
+  "node",
+  "rails"
+
+  $helptexts = @(
+    [pscustomobject]@{name = 'node'; description = 'Includes all possible files in and around the Node System. Mainly used to exclude node_modules and package-locks, but also for framework folders like .next'}
+    [pscustomobject]@{name = 'rails'; description = 'Gitignore for popular Ruby Web Framework Rails. Excludes DBs, Logs, Secrets and more!'}
   )
 
-  $templates = @(
-    [pscustomobject]@{name = 'npm'; content = 'n'}
-  )
+  function showHelp()
+  {
+    Write-Output "gitignore [type]"
+    Write-Output ""
+    Write-Output "Possible Types:"
+
+    $helptexts | ForEach-Object {
+      Write-Output "  $($_.name) - $($_.description)"
+    }
+
+    Write-Output ""
+    Write-Output "Possible Flags:"
+    Write-Output "  -h, --help  Show help"
+    Write-Output "  -s --show %template% Show Content of Template file"
+  }
 
   if ($help -eq $true)
   {
-    Write-Output "gitignore [type]"
-    Write-Output "Possible Types:"
+    showHelp
+    return
+  }
 
-    $possibletypes | ForEach-Object {
-      Write-Output "  $($_.name) - $($_.description)"
+  if ($show -eq $true)
+  {
+    if ($type -ne '')
+    {
+      if ($possibletypes -contains $type)
+      {
+        $template = Get-Content "$PSScriptRoot\templates\$type.txt"
+        Write-Output $template
+      }
     }
     return
   }
 
   if ($type -ne '')
   {
-    Write-Output $type
+    if ($possibletypes -contains $type)
+    {
+      $fileexists = Test-Path ".gitignore"
+
+      if ($fileexists -eq $true)
+      {
+        Write-Error "File .gitignore already exists!"
+        return
+      }
+      $template = Get-Content $PSScriptRoot\templates\$type.txt
+      $template | Out-File .gitignore
+      Write-Output "Successfully created .gitignore with template: $type"
+    } else
+    {
+      Write-Error "Type $type is not a valid type"
+    }
   } else
   {
-    Write-Output 'No type specified'
+    showHelp
   }
 }
